bbfb9ff179892a5cb9bcb8ef0211ca60cfea464f
Two\epicpython <jr3python@gmail.com>
2016-12-12 11:43:34 -0500

    Added "lousy background art" picture and mainmenu scene with quit button

---
+++ Assets/Code/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+//Attach this code to a button
+//The player clicks on the button to quit the game
+//Created by Julie Bodette
+
+//may need to create an actual build of the game to test this
+//generally works in actual builds but not in the unity editor
+
+	public void onClick()
+	{
+		Application.Quit();
+	}
+}
