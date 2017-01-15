24cc041e08292b421859ab8964b453c431abca21
DESKTOP-U2RKT7R\Daggett <andrewdecker3526@gmail>
2017-01-14 21:21:09 -0500

    Creating the Player Prefab
    Created the player and added motion abilities.
    Character movement is handled exclusively in playerMovement
    Created player manager script to hold all player stats in terms of
    health, max health, killing player, movement speed, etc.
    Created debug function that will only run while in the unity editor
    controls are as follows:
    T: Heal Player 5 HP
    G: Damage player 5 HP
    B: Kill Player
    Y: Increase Max Health 5 HP and heal 5 HP
    H: Decrease Max Health 5 HP and remove 5 HP
    U: Increase Movement Speed by 1
    J: Decrease Movement Speed by 1

---
+++ KindSpirit/Assets/Code/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+    private PlayerManager playerManager;
+    private Rigidbody2D playerRB;                   // Used to move the player
+    private Vector2 move;                           // Used to store the input
+    private SpriteRenderer playerSpriteRenderer;    // Used in re-orienting sprite based on movement
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Set up any necesary references
+        playerManager = GetComponent<PlayerManager>();
+        playerRB = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        // Take in movement from the controller and multiply the movement speed by the player speed
+        move.x = playerRB.position.x + (Input.GetAxis("Horizontal") * Time.fixedDeltaTime * playerManager.MovementSpeed);
+        move.y = playerRB.position.y + (Input.GetAxis("Vertical") * Time.fixedDeltaTime * playerManager.MovementSpeed);
+        
+        // Apply motion
+        playerRB.MovePosition(move);
+
+        // Flip Sprite appropriately
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        // Get the input for left/right motion
+        float h = Input.GetAxis("Horizontal");
+
+        // If we aren't moving left or right ..
+        if (h == 0)
+        {
+            // .. don't do anything
+            return;
+        }
+        else // ..otherwise,
+        {
+            // .. change the sprite direction appropriately
+            playerSpriteRenderer.flipX = h > 0;
+        }
+    }
+}
