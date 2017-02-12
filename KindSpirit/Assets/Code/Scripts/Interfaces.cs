942f087072b78df4539afd6248a76134e0717c28
Abraham Blain <ab543512@ohio.edu>
2017-02-12 14:33:49 -0500

    Folders Structured
    Merged Floor Code and Master and updated folder structure

---
+++ KindSpirit/Assets/Code/Scripts/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿public interface IKillable
+{
+    void Kill();
+}
+
+public interface IDamagable<T>
+{
+    void Damage(T damageTaken);
+}
+
+public interface Healable<T>
+{
+    void Heal(T regen);
+}
+
+public interface IInteractable
+{
+    void Interact();
+}
+
+public interface IInteractable<T>
+{
+    void Interact(T interactionParam);
+}
