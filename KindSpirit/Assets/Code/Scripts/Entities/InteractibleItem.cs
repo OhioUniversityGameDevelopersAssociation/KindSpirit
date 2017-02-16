e4608eadbd586656cc1aff7cdfde1704e44a5bcb
Two\epicpython <jr3python@gmail.com>
2017-02-16 17:28:12 -0500

    Added Interactible items
    A transparent "message background" appears when the player touches an
    object (Gets within the trigger collider range) and disappears when the
    player stop touching the object (leaves the trigger collider range).
    
    I just need to change it so that text appears as well.

---
+++ KindSpirit/Assets/Code/Scripts/Entities/InteractibleItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleItem : MonoBehaviour {
+	//public Transform messageprefabtransform;
+	public GameObject messageprefab;
+
+	void Start(){
+		//messageprefabtransform = this.gameObject.transform.GetChild (0);
+		//messageprefab = this.gameObject.transform.GetChild (0).GetChild (0).gameObject;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			Debug.Log ("Player touched object");
+			Instantiate (messageprefab); //set text somehow
+		}
+	}
+	void OnTriggerExit2D(Collider2D other) {
+		// Destroy message
+		Debug.Log ("someone exited collider");
+		if (other.tag == "Player") 
+		{
+			Debug.Log ("player exited collider");
+			GameObject[] messages = GameObject.FindGameObjectsWithTag ("Message"); //Destroy (messageprefab);
+			foreach (GameObject o in messages) {
+				Destroy (o);
+			}
+		}
+	}
+}
