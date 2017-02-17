1ade6b93b8a69bd8bd7757a1007271e0cfc9537a
DESKTOP-U2RKT7R\Daggett <andrewdecker3526@gmail>
2017-02-16 21:52:55 -0500

    Made abstracts necessary for enemies
    Added proper universal enemy manager and enemy scripted behavior
    abstract

---
+++ KindSpirit/Assets/Code/Scripts/Entities/Enemies/EnemyManager.cs
@@ -0,0 +1,65 @@
+﻿/*******************************************************************
+ * 
+ * Created by Andrew Decker
+ * 
+ * Attached to all enemies (might make a different one for the bosses)
+ * Stores all important values for the enemies to be used by their 
+ * scripted behavior. Override this as necessary.
+ * 
+ * ****************************************************************/
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour, IKillable, IDamagable<int>
+{
+    // All fields regarding zombie stats are private
+    // and must only be changed by items / events and
+    // special function calls
+
+    // Enemy Health
+    private int health;
+    public int Health { get { return health; } }
+
+    // Enemy Max Health
+    [SerializeField]
+    private int maxHealth = 10;
+    public int MaxHealth { get { return maxHealth; } }
+
+    // Enemy Movement Speed in units-per-second (UPS)
+    [SerializeField]
+    private float movementSpeed = 5.0f;
+    public float MovementSpeed { get { return movementSpeed; } }
+
+    // Attack power of whatever the enemy's attack may be
+    [SerializeField]
+    private float attackPower = 3;
+    public float AttackPower { get { return attackPower; } }
+
+    protected virtual void Awake()
+    {
+        // Reset enemy health. this may have to change if enemies are stored from one room to another
+        health = maxHealth;
+    }
+
+    public virtual void Kill()
+    {
+        // When overriden, base.Kill() should be the last line in the override
+        // Don't forget to play the death animation first!
+
+        health = 0;
+
+        // Add a timer to this in children scripts to play their death animations
+        Destroy(gameObject);
+    }
+
+    public virtual void Damage(int damageTaken)
+    {
+        health -= damageTaken;
+
+        Debug.Log(name + " damage taken: " + damageTaken + "    " + name + " current Health: " + health);
+
+        if (health <= 0)
+        {
+            Kill();
+        }
+    }
+}
---
+++ KindSpirit/Assets/Code/Scripts/Entities/Enemies/ScriptedEnemyBehaviorAbstract.cs
@@ -0,0 +1,60 @@
+﻿/*********************************************************************
+ * 
+ * Created By Andrew Decker
+ * 
+ * An abstract to lay the groundwork for all enelmies with patterned
+ * behavior. This must be overridden and cannot be directly attached
+ * to an enemy. This will probably need to be updated at we go on.
+ * 
+ * The Behavior loop works as follows
+ * write a behavior for what the enemy does when not attacking and
+ * state 0, then wait before enacting stage 1 of behavior loop and repeat.
+ * Ask me in a meeting if you have questions.
+ * 
+ * ******************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ScriptedEnemyBehaviorAbstract : MonoBehaviour {
+
+    EnemyManager enemyManager;
+
+    protected virtual void Awake()
+    {
+        enemyManager = GetComponent<EnemyManager>();
+    }
+
+    // Use this for initialization
+    protected virtual void Start ()
+    {
+        // Override this if you want an animation or other action to happen exclusively when an enemy enters a room
+        StartCoroutine(BehaviorLoop());
+	}
+
+    protected virtual void Update()
+    {
+        if(enemyManager.Health <= 0)
+        {
+            // Stop the behavior Loop
+            StopAllCoroutines();
+        }
+    }
+
+    protected virtual IEnumerator BehaviorLoop()
+    {
+        // Overwrite this in the inherited classes with replacement behaviors
+
+        do
+        {
+            // Implement Behavior state 1 heres
+            yield return null;
+
+            // Behavior state 2 here
+            yield return null;
+
+            // Implement more states as needed
+        } while (enemyManager.Health > 0);
+    }
+}
