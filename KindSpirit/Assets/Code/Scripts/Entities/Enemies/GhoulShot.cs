174f6499f760f7f7773ce1cd349af45b7f76a461
epicpython <jr3python@gmail.com>
2017-04-25 03:52:03 -0400

    Added Ghoul and Spooky Ghoul
    Animations work, though they only run the first time the ghouls appear.

---
+++ KindSpirit/Assets/Code/Scripts/Entities/Enemies/GhoulShot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhoulShot : MonoBehaviour {
+
+    public float speed;
+    private Transform player;
+    private int lifespan = 3;
+    private Vector2 previousPos;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = (player.transform.position - this.transform.position).normalized * speed;
+        //rb.velocity = transform.up * speed;
+        //this.transform.position = player.transform.position;
+    }
+
+    void Update()
+    {
+        Destroy(this.gameObject, lifespan);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Decreases the players health
+            other.gameObject.GetComponent<PlayerManager>().Damage(1);
+            Destroy(this.gameObject);
+        }
+        else if (other.gameObject.CompareTag("wall"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
